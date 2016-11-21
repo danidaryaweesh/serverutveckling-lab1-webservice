27ea74d1a5a7a81748f3b5f0f4d51a0b875afce5
danidaryaweesh <dani_kurkees@hotmail.com>
2016-11-21 15:00:55 +0100

    first project

---
+++ src/HelloWorld.java
@@ -0,0 +1,18 @@
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+/**
+ * Created by dani on 2016-11-21.
+ */
+
+@Path("/hello")
+public class HelloWorld {
+
+    @GET
+    @Produces("text/plain")
+    public String getClichedMessage() {
+        return "Hello World";
+    }
+
+}
