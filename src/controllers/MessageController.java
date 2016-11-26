241c76f4a8ec4ca6925afbc66c62afe519e01e83
danidaryaweesh <dani_kurkees@hotmail.com>
2016-11-26 13:36:04 +0100

    getMessage working

--- src/controllers/MessageController.java
+++ src/controllers/MessageController.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import Dao.MessageDao;
+import Dao.UserDao;
 import com.google.gson.Gson;
 import model.Message;
 import model.User;
@@ -10,6 +11,8 @@ import service.serviceImpl.MessageServiceImpl;
 import service.serviceImpl.UserServiceImpl;
 
 import javax.ws.rs.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by dani on 2016-11-25.
@@ -21,10 +24,33 @@ public class MessageController {
     private UserService userService;
     private MessageService messageService;
 
+    @Path("/{username}")
+    @GET
+    @Produces
+    public String getMessagesWithUser(@PathParam(value = "username") String username){
+        if(userService == null){
+            userService = new UserServiceImpl();
+        }
+        if(messageService == null){
+            messageService = new MessageServiceImpl();
+        }
+        System.out.println("Getting message..");
+        User user = userService.findUserByUsername(username);
+
+        if(user != null){
+            List<MessageDao> messagesToReturn = getMessageDaos(user.getRecieverMessage());
+            Gson gson = new Gson();
+            String response = gson.toJson(messagesToReturn);
+            return response;
+        }else{
+            return "Empty";
+        }
+    }
+
     @POST
     @Consumes("text/plain")
     @Produces("text/plain")
-    public String getMessage(String messageDao){
+    public String addMessage(String messageDao){
         System.out.println("before ifs..");
         if(userService == null){
             userService = new UserServiceImpl();
@@ -56,5 +82,29 @@ public class MessageController {
         }else{
             return "Empty";
         }
+    }
+
+    private List<MessageDao> getMessageDaos(List<Message> messages){
+        List<MessageDao> listToReturn = new ArrayList<MessageDao>();
+
+        for(int i=0;i<messages.size();i++){
+            MessageDao dao = new MessageDao();
+            dao.setId(messages.get(i).getId());
+            dao.setDate(messages.get(i).getDate() !=null ? messages.get(i).getDate() : null);
+            dao.setContent(messages.get(i).getContent());
+            dao.setSender(convertMessageUserToDao(messages.get(i).getSender()));
+            listToReturn.add(dao);
+        }
+        return listToReturn;
+    }
+
+    private UserDao convertMessageUserToDao(User user){
+        UserDao userDaoToReturn = new UserDao();
+        userDaoToReturn.setId(user.getId());
+        userDaoToReturn.setUsername(user.getUsername());
+        userDaoToReturn.setAddress(user.getAddress());
+        userDaoToReturn.setWorkTitle(user.getWorkTitle());
+        userDaoToReturn.setAge(user.getAge());
+        return userDaoToReturn;
     }
 }
